8945b3dcbcb7cf69ca300ddaeb683db223eadbb8
hayder-ibadi <147252515+hayder-ibadi@users.noreply.github.com>
2025-02-14 16:19:03 +0300

    Update transforms.py

--- utils/transforms.py
+++ utils/transforms.py
@@ -1,23 +1,44 @@
 from torchvision.transforms import (
     Compose,
-    Resize,
+    Normalize,
     RandomRotation,
-    ToTensor,
-    Normalize
+    RandomResizedCrop,
+    RandomHorizontalFlip,
+    RandomAdjustSharpness,
+    Resize,
+    ToTensor
 )
 
-def get_transforms(processor):
-    normalize = Normalize(mean=processor.image_mean, std=processor.image_std)
-    return {
-        "train": Compose([
-            Resize((processor.size["height"], processor.size["width"])),
+def create_transforms(processor):
+    image_mean, image_std = processor.image_mean, processor.image_std
+    size = processor.size["height"]
+
+    normalize = Normalize(mean=image_mean, std=image_std)
+
+    _train_transforms = Compose(
+        [
+            Resize((size, size)),
             RandomRotation(30),
+            RandomAdjustSharpness(2),
             ToTensor(),
             normalize
-        ]),
-        "val": Compose([
-            Resize((processor.size["height"], processor.size["width"])),
+        ]
+    )
+
+    _val_transforms = Compose(
+        [
+            Resize((size, size)),
             ToTensor(),
             normalize
-        ])
-    }
+        ]
+    )
+
+    def train_transforms(examples):
+        examples['pixel_values'] = [_train_transforms(image.convert("RGB")) for image in examples['image']]
+        return examples
+
+    def val_transforms(examples):
+        examples['pixel_values'] = [_val_transforms(image.convert("RGB")) for image in examples['image']]
+        return examples
+
+    return train_transforms, val_transforms
